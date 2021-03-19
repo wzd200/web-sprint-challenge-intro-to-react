a3af59d05886616ea5d533c970851e4ff05064fe
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-03-19 11:02:21 -0400

    Met MVP after adding styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,12 +1,17 @@
 // test Write your Character component here
 import React from 'react'
+import styled from 'styled-components'
 
+const Styledh3 = styled.h3`
+  color: orange;
+  text-shadow: 0 0 3px #FF0000, 0 0 5px #0000FF;
+`
 
 export default function Character(starWarsCharacter) {
     // console.log('character is working')
     // console.log(starWarsCharacter.name)
     return (
         <div>
-            <h3>{`Name: ${starWarsCharacter.name}, Height: ${starWarsCharacter.height}`}</h3>
+            <Styledh3>{`Name: ${starWarsCharacter.name} Height: ${starWarsCharacter.height}`}</Styledh3>
         </div>
       )}
